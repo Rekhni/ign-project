23fc60179da02c00d1a83cc7e260be91e9f4f3c9
Rakhymzhan <mark.john.99@mail.ru>
2025-06-21 00:06:43 +0500

    Updates

--- assets/museumData-DlJEYb17.js
+++
@@ -1,1 +0,0 @@
-const m=[{images:["/museum/museumImg1.webp","/museum/museumImg2.webp","/museum/museumImg3.webp","/museum/museumImg4.webp","/museum/museumImg5.webp","/museum/museumImg6.webp","/museum/museumImg7.webp","/museum/museumImg8.webp"]}];export{m};

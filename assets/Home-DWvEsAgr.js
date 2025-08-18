b58be5cf67dbcc3c4aabb143c83f0a7a199bb1d1
Rakhymzhan <mark.john.99@mail.ru>
2025-08-18 10:51:03 +0500

    Updates

--- assets/Home-DWvEsAgr.js
+++ assets/Home-DWvEsAgr.js
@@ -1,4 +1,4 @@
-import{r as rt,j as f,N as M,C as it}from"./index-DREWNX38.js";import{s as st}from"./logo-CeLyqbyJ.js";import{p as at}from"./partnersData-e1j_7rhw.js";const ot="/ign-project/assets/bg-img5-O9yKKoAS.png";/*!
+import{r as rt,j as f,N as M,C as it}from"./index-BJ96-Lvs.js";import{s as st}from"./logo-CeLyqbyJ.js";import{p as at}from"./partnersData-e1j_7rhw.js";const ot="/ign-project/assets/bg-img5-O9yKKoAS.png";/*!
  * Glide.js v3.7.1
  * (c) 2013-2024 Jędrzej Chałubek (https://github.com/jedrzejchalubek/)
  * Released under the MIT License.
